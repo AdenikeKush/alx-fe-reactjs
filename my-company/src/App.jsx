ddbcc264b4e84129aaf4dbde64a1d238306f31a8
AdenikeKush <kushangel02@gmail.com>
2025-11-09 19:15:53 +0000

    Build my-company site: routing, Navbar, pages, contact form, inline styles; fix single Router setup

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,18 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './Navbar'
-import Home from './Home'
-import About from './About'
-import Services from './Services'
-import Contact from './Contact'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import './App.css';
 
 function App() {
   return (
     <div>
-      {/* Always visible */}
       <Navbar />
 
-      {/* Page content changes by route */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -20,7 +18,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
